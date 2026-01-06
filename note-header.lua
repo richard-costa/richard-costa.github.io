ca1a3fa820bf350900353dd0afe4afa98cbe8744
Richard Costa <scosta.richard@gmail.com>
2026-01-06 10:01:07 -0300

    remove pdf view badge

--- note-header.lua
+++ note-header.lua
@@ -1,31 +1,25 @@
 function Pandoc(doc)
   local meta = doc.meta
-  if meta.status == nil then
+  if not meta.status then
     return doc
   end
 
   local status = pandoc.utils.stringify(meta.status)
 
   local labels = {
-    draft = "DRAFT",
-    wip = "WORK IN PROGRESS",
+    draft  = "DRAFT",
+    wip    = "WORK IN PROGRESS",
     mature = "MATURE"
   }
 
   local label = labels[status]
-  if label == nil then
+  if not label then
     return doc
   end
 
   local html = string.format([[
   <div class="note-header">
     <span class="note-status status-%s">%s</span>
-    <a href="qm_review.pdf"
-      target="_blank"
-      class="note-status note-pdf cta-pdf"
-      title="View PDF"
-      aria-label="View PDF">
-    </a>
   </div>
   ]], status, label)
 
